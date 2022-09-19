717e97f221c9ef6b2500529749d7645eef19290c
Chathuranga97lj <chathuit97@gmail.com>
2022-09-19 17:31:34 +0530

    add route handling

--- app.js
+++ app.js
@@ -2,4 +2,22 @@ const express = require('express');
 
 const app = express();
 
+// routes
+app.get('/', (req, res, next) => {
+    // normal respons
+    // res.status(200).send('Welcome to the Home Page');
+
+    // json response
+    // res.json({
+    //     message: "welcome to the home page"
+    // })
+
+    // redirect some another path
+    res.redirect('/user')
+});
+
+app.get('/user', (req, res, next) => {
+    res.status(200).send('Welcome to the user page');
+})
+
 module.exports = app;
