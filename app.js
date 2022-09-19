22bef8f3db595a530b9f24475f4f2b3d913cd628
Chathuranga97lj <chathuit97@gmail.com>
2022-09-19 22:15:05 +0530

    add request params and query

--- app.js
+++ app.js
@@ -13,11 +13,13 @@ app.get('/', (req, res, next) => {
     // })
 
     // redirect some another path
-    res.redirect('/user')
+    res.send('Welcome to homepage');
 });
 
-app.get('/user', (req, res, next) => {
-    res.status(200).send('Welcome to the user page');
+app.get('/user/:id/:postId', (req, res, next) => {
+    console.log(req.query); // /10?comment=first  after the ? part in url /  &like=first
+    //console.log(req.params); // http://localhost:5000/user/1/10  -- after user/ the sections called params like /1/10
+    res.status(200).send('Welcome to the user page in nodemon');
 })
 
 module.exports = app;
