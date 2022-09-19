4ce9e1a08c1d7c3f6675dec003c73cda673baa41
Chathuranga97lj <chathuit97@gmail.com>
2022-09-19 22:39:07 +0530

    add req object and middleware

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+// middlewares
+app.use((req, res, next) => {
+    console.log(req.ip);
+    next(); // pass req in middleware to routers
+});
+
 // routes
 app.get('/', (req, res, next) => {
     // normal respons
@@ -17,8 +23,10 @@ app.get('/', (req, res, next) => {
 });
 
 app.get('/user/:id/:postId', (req, res, next) => {
-    console.log(req.query); // /10?comment=first  after the ? part in url /  &like=first
+    //console.log(req.query); // /10?comment=first  after the ? part in url /  &like=first
     //console.log(req.params); // http://localhost:5000/user/1/10  -- after user/ the sections called params like /1/10
+    const host = req.get('Host');
+    console.log(host);
     res.status(200).send('Welcome to the user page in nodemon');
 })
 
