73330266389d472517a3aee556160a09eb52fc36
Chathuranga97lj <chathuit97@gmail.com>
2022-09-20 23:04:33 +0530

    set pagination

--- configuration/db.js
+++ configuration/db.js
@@ -1,6 +1,6 @@
-const {MongoClient, Db} = require('mongodb');
+const {MongoClient} = require('mongodb');
 
-const _uri = 'mongodb+srv://shop-user:32706189@cluster0.rbwuf5l.mongodb.net/?retryWrites=true&w=majority'
+const _uri = 'mongodb+srv://shop-user:32706189@cluster0.rbwuf5l.mongodb.net/sample_mflix?retryWrites=true&w=majority'
 
 const dbCon = (coll, cb) => {
     MongoClient.connect(_uri)
