78ca5c6a56bab568185bd11f7b8c46db8ae55748
Chathuranga97lj <chathuit97@gmail.com>
2022-09-20 13:11:30 +0530

    set auth router

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,5 @@
+const authRouter = require('./auth');
+
 module.exports = (app) => {
-    // routes
-    app.get('/', (req, res, next) => {
-        // normal respons
-        // res.status(200).send('Welcome to the Home Page');
-
-        // json response
-        // res.json({
-        //     message: "welcome to the home page"
-        // })
-
-        // redirect some another path
-        res.send('Welcome to homepage');
-    });
-
-    app.get('/user/:id/:postId', (req, res, next) => {
-        //console.log(req.query); // /10?comment=first  after the ? part in url /  &like=first
-        //console.log(req.params); // http://localhost:5000/user/1/10  -- after user/ the sections called params like /1/10
-        const host = req.get('Host');
-        console.log(host);
-        res.status(200).send('Welcome to the user page in nodemon');
-    })
+    app.use('/auth', authRouter); 
 }
