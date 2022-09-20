e56c247d72bc38b13334a6d1e38ad381c4e1a432
Chathuranga97lj <chathuit97@gmail.com>
2022-09-20 17:41:47 +0530

    config db

---
+++ configuration/db.js
@@ -0,0 +1,15 @@
+const {MongoClient} = require('mongodb');
+
+const _uri = 'mongodb+srv://shop-user:32706189@cluster0.rbwuf5l.mongodb.net/?retryWrites=true&w=majority'
+
+const dbCon = (coll, cb) => {
+    MongoClient.connect(_uri)
+        .then(async client => {
+            const db = client.db('sample_mflix').collection(coll);
+            await cb(db);
+            client.close();
+        })
+        .catch()
+};
+
+module.exports = dbCon;
